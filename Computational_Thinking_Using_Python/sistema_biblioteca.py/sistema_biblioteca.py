1c9a1d6321c00392a94af0506badc2cc6564c7a2
Joao-Dire <vitordire@gmail.com>
2024-04-05 20:41:05 -0300

    Add files via upload

---
+++ Computational_Thinking_Using_Python/sistema_biblioteca.py/sistema_biblioteca.py
@@ -0,0 +1,7 @@
+
+while opcao != 6:
+print("Bem-vindo à Biblioteca FIAP")
+print("1 - Resera Livro \n2 - Cadastrar Livro")
+print("3 - Devolução \n4 = Consulta Livro")
+print("5 - Cadastro do Leitor \n6 - Sair")
+opcao = int(input("Digite a opção desejada"))
